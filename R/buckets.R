6e591b9ab2320a238319b1617fce03a30c38c756
Mark Edmondson <github@markedmondson.me>
2016-04-03 11:52:52 +0200

    Update buckets, create working

--- R/buckets.R
+++ R/buckets.R
@@ -84,7 +84,7 @@ gcs_get_bucket <- function(bucket,
 gcs_create_bucket <-
   function(name,
            projectId,
-           location = "us",
+           location = "US",
            storageClass = c("STANDARD",
                             "NEARLINE",
                             "DURABLE_REDUCED_AVAILABILITY"),
@@ -131,7 +131,98 @@ gcs_create_bucket <-
 
   req <- bb(the_body = body)
 
-  req$content
+  if(req$status_code == 200){
+    myMessage("Bucket created successfully:", name, " in ", location, level = 3)
+    out <- req$content
+  } else {
+    myMessage("Bucket creation failed:", name, " in ", location, level = 3)
+    out <- FALSE
+  }
+
+  out
 
 }
+
+#' Update a bucket
+#'
+#' @param bucket Name of the bucket
+#' @param new_name Globally unique name of bucket
+#' @param ifMetagenerationMatch Return only if metageneration matches
+#' @param ifMetagenerationNotMatch Return only if metageneration does not match
+#' @param location Location of bucket. See details
+#' @param storageClass Type of bucket
+#' @param predefinedAcl Apply predefined access controls to bucket
+#' @param predefinedDefaultObjectAcl Apply predefined access controls to objects
+#' @param projection Properties to return. Default noAcl omits acl properties
+#'
+#' @details
+#'   \href{https://cloud.google.com/storage/docs/bucket-locations}{See here for details on location options}
+#'
+#' todo: acl, cors, lifecycle, logging, versioning, website
+#'
+#' @export
+gcs_update_bucket <-
+  function(bucket,
+           new_name = NULL,
+           ifMetagenerationMatch = NULL,
+           ifMetagenerationNotMatch = NULL,
+           location = NULL,
+           storageClass = c("STANDARD",
+                            "NEARLINE",
+                            "DURABLE_REDUCED_AVAILABILITY"),
+           predefinedAcl = c("authenticatedRead",
+                             "private",
+                             "projectPrivate",
+                             "publicRead",
+                             "publicReadWrite"),
+           predefinedDefaultObjectAcl = c("authenticatedRead",
+                                          "private",
+                                          "projectPrivate",
+                                          "publicRead",
+                                          "publicReadWrite"),
+           projection = c("noAcl","full")){
+
+    projection    <- match.arg(projection)
+    predefinedAcl <- match.arg(predefinedAcl)
+    storageClass  <- match.arg(storageClass)
+    predefinedDefaultObjectAcl <- match.arg(predefinedDefaultObjectAcl)
+
+    testthat::expect_is(bucket, "character")
+    testthat::expect_is(location, "character")
+    testthat::expect_is(projection, "character")
+
+    pars_args <- list(ifMetagenerationMatch=ifMetagenerationMatch,
+                      ifMetagenerationNotMatch=ifMetagenerationNotMatch,
+                      predefinedAcl = predefinedAcl,
+                      predefinedDefaultObjectAcl = predefinedDefaultObjectAcl,
+                      projection = projection)
+    pars_args <- rmNullObs(pars_args)
+
+    bb <-
+      googleAuthR::gar_api_generator("https://www.googleapis.com/storage/v1/",
+                                     "POST",
+                                     path_args = list(b = bucket),
+                                     pars_args = pars_args)
+
+    body <- list(
+      name = new_name,
+      location = location,
+      storageClass = storageClass
+    )
+
+    body <- rmNullObs(body)
+
+    req <- bb(the_body = body)
+
+    if(req$status_code == 200){
+      myMessage("Bucket update successfully", level = 3)
+      out <- req$content
+    } else {
+      myMessage("Bucket update failed", level = 3)
+      out <- FALSE
+    }
+
+    out
+
+  }
 
