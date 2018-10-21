2ca05cdc01e0900af3d14a5d31f1e09c8bea5fcc
Jas Sohi <j450h1@gmail.com>
2018-10-21 03:50:00 -0700

    Finalize 'list' action for bucket versioning

--- R/versioning.R
+++ R/versioning.R
@@ -1,93 +1,90 @@
 #' Turn bucket versioning on or off, check status (default), or
-#' list both live and archived versions of objects in the bucket 
-#' and view their generation numbers. Archived versions of objects have a 
-#' `timeDeleted` property. 
+#' list archived versions of objects in the bucket and view their generation numbers.
+#'
 #'
 #' @param action "status", "enable", "disable", or "list"
 #' @param bucket gcs bucket
 #'
-#' @return versioned_objects list #only if "list" action
+#' @return versioned_objects dataframe #only if "list" action
 #' @importFrom googleAuthR gar_api_generator
 #' @family managementAPI functions
 #' @export
-gcs_version_bucket <- function(bucket, action = c("status","enable","disable","list")) {
-  
+gcs_version_bucket <- function(bucket, action = c("status", "enable", "disable", "list")) {
   action <- match.arg(action)
-  
+
   bucket <- as.bucket_name(bucket)
-  
+
   # check that it is a valid bucket that user has access to
   tryCatch({
     gcs_get_bucket(bucket)
     # cat(sprintf("\n \"%s\" is a valid bucket.\n", bucket))
   }, error = function(e) {
     stop("Bucket not found. Check bucket name and if you have read permissions.
-    Looked for ", bucket,  call. = FALSE)
+    Looked for ", bucket, call. = FALSE)
   })
-  
+
   # will only be different for list
   url <- sprintf(
     "https://www.googleapis.com/storage/v1/b/%s",
     bucket
   )
   pars_args <- list(fields = "versioning")
-  
+
   # Default is to check status
   if (action == "status") {
-    
     api <- googleAuthR::gar_api_generator(url,
-                                          "GET",
-                                          pars_args = pars_args,
-                                          data_parse_function = function(x) x,
-                                          checkTrailingSlash = FALSE
+      "GET",
+      pars_args = pars_args,
+      data_parse_function = function(x) x,
+      checkTrailingSlash = FALSE
     )
-    
-    if (length(api()) == 0) { #If versioning has never been enabled before
+
+    if (length(api()) == 0) { # If versioning has never been enabled before
       cat(sprintf("Versioning is NOT ENABLED for \"%s\"", bucket))
     } else {
       if (api()$versioning$enabled == TRUE) {
         cat(sprintf("Versioning is ENABLED for \"%s\"", bucket))
-      }
-      else {
+      } else {
         cat(sprintf("Versioning is NOT ENABLED for \"%s\"", bucket))
       }
     }
   }
   # on or off
-  else if (action %in% c("enable","disable")) {
-    
+  else if (action %in% c("enable", "disable")) {
     trueorfalse <- ifelse(action == "enable", TRUE, FALSE)
-    
+
     body <- list(versioning = list(enabled = trueorfalse))
-    
+
     api <- googleAuthR::gar_api_generator(url,
-                                          "PATCH",
-                                          pars_args = pars_args,
-                                          data_parse_function = function(x) x,
-                                          checkTrailingSlash = FALSE
+      "PATCH",
+      pars_args = pars_args,
+      data_parse_function = function(x) x,
+      checkTrailingSlash = FALSE
     )
-    
+
     api(the_body = body)
-    
   }
   else if (action == "list") {
-    # To list both live and archived versions of an object and view their generation numbers:
+    # To list both live and archived versions of an object and view their generation numbers 
+    # (filter only archived versions for final output)
     # Archived versions of objects have a `timeDeleted` property.
-    
+
     url <- sprintf(
       "https://www.googleapis.com/storage/v1/b/%s/o",
       bucket
     )
     pars_args <- list(versions = "true")
-    
+
     api <- googleAuthR::gar_api_generator(url,
-                                          "GET",
-                                          pars_args = pars_args,
-                                          data_parse_function = function(x) x$items,
-                                          checkTrailingSlash = FALSE
+      "GET",
+      pars_args = pars_args,
+      data_parse_function = function(x) x$items,
+      checkTrailingSlash = FALSE
     )
-    
-    api()
-    
+    if (!("timeDeleted" %in% colnames(api()))) {
+      stop("`timeDeleted` property not found. No archived versions currently in bucket: ", bucket, call. = FALSE)
+    } else {
+      dplyr::filter(api(), !is.na(timeDeleted))
+    }
   }
 }
