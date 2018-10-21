a43e66b0ec7b7c036bb71ae8ee0462167f43434f
Jas Sohi <j450h1@gmail.com>
2018-10-21 00:54:49 -0700

    Remove extra comments

--- R/versioning.R
+++ R/versioning.R
@@ -55,38 +55,18 @@ gcs_version_bucket <- function(bucket, action = "status") {
     
     trueorfalse <- ifelse(action == "enable", "true", "false")
     
-    #https://stackoverflow.com/questions/42356834/how-to-post-multipart-binary-non-binary-with-httr-for-salesforce-api
-    # x <- as.character(jsonlite::toJSON(sprintf('{
-    #   "versioning": {
-    #     "enabled": %s
-    #   }
-    # }', trueorfalse)))
-    # 
-    # metadata <- curl::form_data(x,"application/json")
-    
-    #body <- list(metadata = metadata)
-    
-    #body = list(y = upload_file(metadata))
-    
     body <- list(x = jsonlite::toJSON(sprintf('{
       "versioning": {
         "enabled": %s
       }
     }', trueorfalse)))
     
-    # body <- list(y = upload_file(system.file('{
-    #   "versioning": {
-    #     "enabled": true
-    #   }
-    # }')))
-    # 
     api <- googleAuthR::gar_api_generator(url,
                                           "PATCH",
                                           pars_args = pars_args,
                                           data_parse_function = function(x) x,
-                                          checkTrailingSlash = FALSE,
+                                          checkTrailingSlash = FALSE
 )
-    )
     
     api(the_body = body)
     
