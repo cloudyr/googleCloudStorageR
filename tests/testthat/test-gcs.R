905a43f355912b772f1cdfa4b0c01fe7565d1730
Mark IIH <mark@iihnordic.com>
2019-07-29 15:53:06 +0200

    tests

--- tests/testthat/test-gcs.R
+++ tests/testthat/test-gcs.R
@@ -1,13 +1,24 @@
-context("Authentication")
+context("googleCloudStorageR")
 
 test_that("Authentication", {
   googleAuthR::skip_if_no_env_auth(
     "GCS_AUTH_FILE"
   )
   expect_true(file.exists(Sys.getenv("GCS_AUTH_FILE")))
+  
+  # auto-auth
+  expect_true(googleAuthR::gar_has_token())
+  
+  gcs_auth(Sys.getenv("GCS_AUTH_FILE"))
+  
+  # manual auth
   expect_true(googleAuthR::gar_has_token())
 })
 
-test_that("Download", {
-  ''
+test_that("Buckets", {
+  b <- gcs_list_buckets("iih-tools-analytics")
+  expect_s3_class(b, "data.frame")
+  expect_true(
+    all(names(b) %in% c("name","storageClass","location","updated"))
+  )
 })
